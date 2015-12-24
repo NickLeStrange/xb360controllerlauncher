a1d11fa6c86ad28ff802cdb780f04b13d72f910b
NickLeStrange <nick.m.green@gmail.com>
2015-12-25 00:12:20 +0200

    Xbox 360 Controller Launcher
    A basic launcher create to be used with a wireless Xbox 360 controller.
    
    Made currently to launch Steam and Kodi.

---
+++ xb360Launcher/frmMain.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnKodi = New System.Windows.Forms.Button()
+        Me.LaunchButtons = New System.Windows.Forms.ImageList(Me.components)
+        Me.btnSteam = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblLaunchWhat = New System.Windows.Forms.Label()
+        Me.tmrControllerDetect = New System.Windows.Forms.Timer(Me.components)
+        Me.pnlBackground = New System.Windows.Forms.Panel()
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.pnlBackground.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnKodi
+        '
+        Me.btnKodi.BackColor = System.Drawing.Color.Transparent
+        Me.btnKodi.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue
+        Me.btnKodi.FlatAppearance.BorderSize = 10
+        Me.btnKodi.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnKodi.ImageIndex = 0
+        Me.btnKodi.ImageList = Me.LaunchButtons
+        Me.btnKodi.Location = New System.Drawing.Point(300, 9)
+        Me.btnKodi.Name = "btnKodi"
+        Me.btnKodi.Size = New System.Drawing.Size(169, 168)
+        Me.btnKodi.TabIndex = 2
+        Me.btnKodi.UseVisualStyleBackColor = False
+        '
+        'LaunchButtons
+        '
+        Me.LaunchButtons.ImageStream = CType(resources.GetObject("LaunchButtons.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.LaunchButtons.TransparentColor = System.Drawing.Color.Transparent
+        Me.LaunchButtons.Images.SetKeyName(0, "Kodi_Button.png")
+        Me.LaunchButtons.Images.SetKeyName(1, "Kodi_Button_Highlight.png")
+        Me.LaunchButtons.Images.SetKeyName(2, "Steam_Button.png")
+        Me.LaunchButtons.Images.SetKeyName(3, "Steam_Button_Highlight.png")
+        '
+        'btnSteam
+        '
+        Me.btnSteam.BackColor = System.Drawing.Color.Transparent
+        Me.btnSteam.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue
+        Me.btnSteam.FlatAppearance.BorderSize = 10
+        Me.btnSteam.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnSteam.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSteam.ImageIndex = 2
+        Me.btnSteam.ImageList = Me.LaunchButtons
+        Me.btnSteam.Location = New System.Drawing.Point(67, 9)
+        Me.btnSteam.Name = "btnSteam"
+        Me.btnSteam.Size = New System.Drawing.Size(169, 168)
+        Me.btnSteam.TabIndex = 1
+        Me.btnSteam.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue
+        Me.btnExit.FlatAppearance.BorderSize = 5
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnExit.Location = New System.Drawing.Point(414, 12)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(106, 49)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblLaunchWhat
+        '
+        Me.lblLaunchWhat.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLaunchWhat.Location = New System.Drawing.Point(12, 274)
+        Me.lblLaunchWhat.Name = "lblLaunchWhat"
+        Me.lblLaunchWhat.Size = New System.Drawing.Size(508, 36)
+        Me.lblLaunchWhat.TabIndex = 0
+        Me.lblLaunchWhat.Text = "..."
+        Me.lblLaunchWhat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tmrControllerDetect
+        '
+        Me.tmrControllerDetect.Enabled = True
+        '
+        'pnlBackground
+        '
+        Me.pnlBackground.BackColor = System.Drawing.Color.DimGray
+        Me.pnlBackground.Controls.Add(Me.btnSteam)
+        Me.pnlBackground.Controls.Add(Me.btnKodi)
+        Me.pnlBackground.Location = New System.Drawing.Point(0, 76)
+        Me.pnlBackground.Name = "pnlBackground"
+        Me.pnlBackground.Size = New System.Drawing.Size(534, 187)
+        Me.pnlBackground.TabIndex = 5
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon.Text = "Xb360 Launcher"
+        Me.NotifyIcon.Visible = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(532, 324)
+        Me.Controls.Add(Me.lblLaunchWhat)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.pnlBackground)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmMain"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.pnlBackground.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnKodi As System.Windows.Forms.Button
+    Friend WithEvents btnSteam As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblLaunchWhat As System.Windows.Forms.Label
+    Friend WithEvents LaunchButtons As System.Windows.Forms.ImageList
+    Friend WithEvents tmrControllerDetect As System.Windows.Forms.Timer
+    Friend WithEvents pnlBackground As System.Windows.Forms.Panel
+    Friend WithEvents NotifyIcon As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ xb360Launcher/frmMain.vb
@@ -0,0 +1,267 @@
+﻿Imports Microsoft.Xna.Framework
+Imports Microsoft.Xna.Framework.Input
+
+Public Class frmMain
+
+#Region "DECLARES"
+
+    'Set Steam and Kodi as processes, and create Booleans for them to be used.
+    Dim Steam() As Process, Kodi() As Process
+    Dim SteamIsRunning As Boolean, KodiIsRunning As Boolean, ControllerConnected As Boolean, StartPressed As Boolean, _
+        DPadLeft As Boolean, DPadRight As Boolean, ButtonAPressed As Boolean
+    'This integer helps to keep track of the currently focused button for navigation.
+    Dim CurrentButton As Integer
+
+#End Region
+
+#Region "CHECK IF RUNNING"
+
+    Private Sub CheckIfRunning()
+        'Check if either of the processes is running.
+        'This will be used to make sure that when the start button on the controller is 
+        'pressed that it won't interfere with your game.
+        Steam = Process.GetProcessesByName("Steam")
+        Kodi = Process.GetProcessesByName("Kodi")
+
+        If Steam.Count > 0 Then
+            'If Steam is running then set the Boolean to True
+            SteamIsRunning = True
+        Else
+            'If not, set the Boolean to False.
+            SteamIsRunning = False
+        End If
+
+        If Kodi.Count > 0 Then
+            'If Kodi is running then set the Boolean to True
+            KodiIsRunning = True
+        Else
+            'If not, set the Boolean to False.
+            KodiIsRunning = False
+        End If
+    End Sub
+
+#End Region
+
+#Region "FORM CONTROL"
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Load up whatever needs to be loaded.
+        ButtonBorders()
+    End Sub
+
+    Private Sub frmMain_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        btnSteam.Focus()
+        Me.Visible = False
+    End Sub
+
+#End Region
+
+#Region "FORM FADES"
+
+    Public Sub FadeFormIn()
+        'This fades the form in when it is shown on the screen.
+        Try
+            For FadeIn = 0.0 To 1.1 Step 0.1
+                Me.Opacity = FadeIn
+                Me.Refresh()
+                Threading.Thread.Sleep(20)
+            Next
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+#End Region
+
+#Region "BUTTONS"
+
+    Public Sub ButtonBorders()
+        'Manage the borders of the buttons on load.
+        btnExit.FlatAppearance.BorderSize = 0
+        btnKodi.FlatAppearance.BorderSize = 0
+        btnSteam.FlatAppearance.BorderSize = 0
+    End Sub
+
+    Private Sub btnSteam_Click(sender As Object, e As EventArgs) Handles btnSteam.Click
+        'Start Steam. This command won't actually force it into big picture if it is already running.
+        'You have to configure Steam to start in Big Picture Mode when it is launched.
+        'You always have to exit Steam completely.
+        Process.Start("C:\Program Files (x86)\Steam\steam.exe", "-bigpicture")
+        Me.Visible = False
+        NotifyIcon.Visible = True
+    End Sub
+
+    Private Sub btnSteam_GotFocus(sender As Object, e As EventArgs) Handles btnSteam.GotFocus
+        'If the Steam button gets focus, set the other buttons accordingly.
+        CurrentButton = 1
+        btnSteam.FlatAppearance.BorderSize = 10
+        btnKodi.FlatAppearance.BorderSize = 0
+        btnExit.FlatAppearance.BorderSize = 0
+        lblLaunchWhat.Text = "Press A to launch STEAM"
+        btnSteam.Image = LaunchButtons.Images.Item(3)
+        btnKodi.Image = LaunchButtons.Images.Item(0)
+    End Sub
+
+    Private Sub btnKodi_Click(sender As Object, e As EventArgs) Handles btnKodi.Click
+        'Start Kodi.
+        Process.Start("C:\Program Files (x86)\Kodi\Kodi.exe")
+        Me.Visible = False
+        NotifyIcon.Visible = True
+    End Sub
+
+    Private Sub btnKodi_GotFocus(sender As Object, e As EventArgs) Handles btnKodi.GotFocus
+        'If the Kodi button gets focus, set the other buttons accordingly.
+        CurrentButton = 2
+        btnKodi.FlatAppearance.BorderSize = 10
+        btnSteam.FlatAppearance.BorderSize = 0
+        btnExit.FlatAppearance.BorderSize = 0
+        lblLaunchWhat.Text = "Press A to launch KODI"
+        btnSteam.Image = LaunchButtons.Images.Item(2)
+        btnKodi.Image = LaunchButtons.Images.Item(1)
+    End Sub
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Exit
+        Me.Close()
+    End Sub
+
+    Private Sub btnExit_GotFocus(sender As Object, e As EventArgs) Handles btnExit.GotFocus
+        'If the Exit button gets focus, set the other buttons accordingly.
+        CurrentButton = 3
+        btnExit.FlatAppearance.BorderSize = 5
+        btnSteam.FlatAppearance.BorderSize = 0
+        btnKodi.FlatAppearance.BorderSize = 0
+        lblLaunchWhat.Text = "Press A to QUIT"
+        btnKodi.Image = LaunchButtons.Images.Item(0)
+        btnSteam.Image = LaunchButtons.Images.Item(2)
+    End Sub
+
+#End Region
+
+#Region "TIMERS"
+
+    Private Sub tmrControllerDetect_Tick(sender As Object, e As EventArgs) Handles tmrControllerDetect.Tick
+        'Set up the xbox 360 controller buttons.
+        Dim currentState As GamePadState = GamePad.GetState(PlayerIndex.One)
+
+        'Check if Steam or Kodi is running.
+        CheckIfRunning()
+
+        If SteamIsRunning = True Then
+            'Steam is running
+        ElseIf KodiIsRunning = True Then
+            'Kodi is running
+        Else
+            'Neither Steam nor Kodi is running, so make use of the buttons.
+            If currentState.IsConnected Then
+                If ControllerConnected = False Then
+                    ControllerConnected = True
+                    Me.Visible = True
+                    FadeFormIn()
+                End If
+
+                'Use the A button on the controller to launch the apps.
+                If currentState.Buttons.A = ButtonState.Pressed Then
+                    lblLaunchWhat.Text = "LAUNCHING SELECTION"
+                    ButtonAPressed = True
+                ElseIf currentState.Buttons.A = ButtonState.Released Then
+                    If ButtonAPressed = True Then
+                        'Simulate the pressing of the Enter key to press the button.
+                        SendKeys.Send("{ENTER}")
+                        ButtonAPressed = False
+                    End If
+                End If
+
+                    'Use the DPAD to go left.
+                    If currentState.DPad.Left = ButtonState.Pressed Then
+                        DPadLeft = True
+                    ElseIf currentState.DPad.Left = ButtonState.Released Then
+                        If DPadLeft = True Then
+                            If CurrentButton = 1 Then
+                                btnExit.Focus()
+                                DPadLeft = False
+                            ElseIf CurrentButton = 2 Then
+                                btnSteam.Focus()
+                                DPadLeft = False
+                            ElseIf CurrentButton = 3 Then
+                                btnKodi.Focus()
+                                DPadLeft = False
+                            End If
+                        End If
+                    End If
+
+                    'Use the DPAD to go Right.
+                    If currentState.DPad.Right = ButtonState.Pressed Then
+                        DPadRight = True
+                    ElseIf currentState.DPad.Right = ButtonState.Released Then
+                        If DPadRight = True Then
+                            If CurrentButton = 1 Then
+                                btnKodi.Focus()
+                                DPadRight = False
+                            ElseIf CurrentButton = 2 Then
+                                btnExit.Focus()
+                                DPadRight = False
+                            ElseIf CurrentButton = 3 Then
+                                btnSteam.Focus()
+                                DPadRight = False
+                            End If
+                        End If
+                    End If
+
+                    'Use the Start button to show the launcher.
+                    If currentState.Buttons.Start = ButtonState.Pressed Then
+                        StartPressed = True
+                    ElseIf currentState.Buttons.Start = ButtonState.Released Then
+                        If StartPressed = True Then
+                            If Me.Visible = True Then
+                                Me.Visible = False
+                                NotifyIcon.Visible = True
+                                StartPressed = False
+                            ElseIf Me.Visible = False Then
+                                Me.Visible = True
+                                FadeFormIn()
+                                NotifyIcon.Visible = False
+                                StartPressed = False
+                            End If
+                        End If
+                    End If
+
+                    'Use the left analogue stick to select between apps.
+                    If currentState.ThumbSticks.Left.X = 1 Then
+
+                        'When pressing left on the analogue stick it cycles through the buttons to the left.
+                        If CurrentButton = 1 Then
+                            btnKodi.Focus()
+
+                        ElseIf CurrentButton = 2 Then
+                            btnExit.Focus()
+
+                        ElseIf CurrentButton = 3 Then
+                            btnSteam.Focus()
+
+                        End If
+                    ElseIf currentState.ThumbSticks.Left.X = -1 Then
+                        'When pressing left on the analogue stick it cycles through the buttons to the right.
+                        If CurrentButton = 1 Then
+                            btnExit.Focus()
+
+                        ElseIf CurrentButton = 2 Then
+                            btnSteam.Focus()
+
+                        ElseIf CurrentButton = 3 Then
+                            btnKodi.Focus()
+
+                        End If
+                    End If
+
+            Else
+                ControllerConnected = False
+                Me.Visible = False
+            End If
+        End If
+    End Sub
+
+#End Region
+
+End Class
